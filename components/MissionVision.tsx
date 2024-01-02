0e6fade9ca2acfb4f65b17fccade0695c4e08c91
belaymit <2belamit@gmail.com>
2024-01-03 00:10:20 +0300

    Added Mission and Vision

--- components/MissionVision.tsx
+++ components/MissionVision.tsx
@@ -6,18 +6,18 @@ return (
     <h2 className="text-3xl text-center font-semibold mb-8">Our Mission and Vision</h2>
 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-      <div className="bg-white p-6 rounded-md shadow-md transition ease-in-out delay-150 transform hover:scale-105 hover:border-blue-500 hover:border-4">
+      <div className="bg-white p-6 rounded-md shadow-md transition ease-in-out delay-150 hover:border-blue-200 hover:border-2">
         <h3 className="text-xl font-semibold mb-4">Our Mission</h3>
         <p className="text-gray-600">
-          To deliver innovative and reliable technology solutions that empower businesses and individuals to thrive in the digital era.
+        MichuTech IT Solutions is dedicated to empowering businesses through innovative technology solutions In Ethiopia and beyond. Our mission is to deliver exceptional website development, software development, system development, and graphic design services globally. We strive to enhance our clients' business performance by providing high-quality, tailored solutions that meet their unique needs. Our commitment is to contribute to the growth and success of businesses by leveraging the power of technology and fostering long-term partnerships.
         </p>
       </div>
 
       {/* Vision */}
-      <div className="bg-white p-6 rounded-md shadow-md transition ease-in-out delay-150 transform hover:scale-105 hover:border-blue-500 hover:border-4">
+      <div className="bg-white p-6 rounded-md shadow-md transition ease-in-out delay-150 hover:border-blue-200 hover:border-2">
         <h3 className="text-xl font-semibold mb-4">Our Vision</h3>
         <p className="text-gray-600">
-          To be a global leader in technology, driving positive change through cutting-edge solutions, exceptional talent, and sustainable practices.
+        MichuTech IT Solutions aims to lead the technology solutions industry, recognized for innovation, quality, and client satisfaction. We strive to be the go-to partner for businesses seeking excellence in website development, software development, system development, and graphic design services.
         </p>
       </div>
     </div>
