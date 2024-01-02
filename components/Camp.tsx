16729c9425f6bb95b0bb8684f6f8e415c482dcdd
belaymit <2belamit@gmail.com>
2024-01-03 00:09:16 +0300

    Modified Camp background images

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -13,7 +13,7 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
     <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
       <div className="flexCenter gap-4">
-        <div className="rounded-full bg-green-50 p-4">
+        <div className="rounded-full bg-blue-800 p-4">
           <Image
             src="/folded-map.svg"
             alt="map"
@@ -22,25 +22,9 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
           />
         </div>
         <div className="flex flex-col gap-1">
-          <h4 className="bold-18 text-white">{title}</h4>
-          <p className="regular-14 text-white">{subtitle}</p>
+          <h4 className="bold-18 text-black">{title}</h4>
+          <p className="regular-14 text-black">{subtitle}</p>
         </div>
-      </div>
-
-      <div className="flexCenter gap-6">
-        <span className="flex -space-x-4 overflow-hidden">
-          {PEOPLE_URL.map((url) => (
-            <Image 
-              className="inline-block h-10 w-10 rounded-full"
-              src={url}
-              key={url}
-              alt="person"
-              width={52}
-              height={52}
-            />
-          ))}
-        </span>
-        <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
       </div>
      </div>
     </div>
@@ -53,25 +37,25 @@ export default function Camp () {
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite 
           backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
-          peopleJoined="50+ Joined"
+          title="MichuTech Washlink"
+          subtitle="Washlink, Wear Clean"
+          peopleJoined=""
         />
         <CampSite 
           backgroundImage="bg-bg-img-2"
-          title="Mountain View Camp"
-          subtitle="Somewhere in the Wilderness"
+          title="MichuGojo"
+          subtitle="Get your next hour at MichuGojo"
           peopleJoined="50+ Joined"
         />
       </div>
 
       <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
-        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+        <div className="bg-blue-900 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
           <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
-            <strong>Feeling Lost</strong> And Not Knowing The Way?
+            <strong>Want to develop any Software</strong> And Not Knowing The Way?
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">
-            Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure
+          Whether you're taking your first steps or you're a seasoned developer seeking innovative solutions, our dedicated team of experts is prepared to collaborate with you. From concept to execution, we're committed to transforming your ideas into powerful, functional software that meets your unique needs.
           </p>
           <Image 
             src="/quote.svg"
