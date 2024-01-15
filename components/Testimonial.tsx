5f9d18d8f1a673f4aaecaa78587ab2557ab25db3
belaymit <2belamit@gmail.com>
2024-01-15 14:00:07 +0300

    Modified grid styles

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -15,9 +15,9 @@ interface Testimonial {
 export default function Testimonials() {
   return (
     <section className="bg-gray-100 py-16 flex items-center">
-    <div className="container mx-auto flex flex-col items-center justify-center"> {/* Updated this line */}
+    <div className="container mx-auto flex flex-col items-center justify-center"> 
       <h2 className="text-3xl font-bold text-center mb-8">What People Clients Say About MichuTech?</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 justify-center">
         {testimonialsData.map((testimonial) => (
           <div key={testimonial.id} className="bg-white p-6 rounded-lg">
             <div className="flex items-cn mb-4">
