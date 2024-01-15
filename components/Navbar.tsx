b2e2572b477aa2cf5ebc9ebcdd359c1ee0d30bed
belaymit <2belamit@gmail.com>
2024-01-15 13:59:45 +0300

    Changed logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,18 +14,21 @@ export default function Navbar () {
   };
 
   return (
-   <nav className="flexBetween padding-container fixed top-0 w-full left-0 right-0 z-30 py-5 bg-white" style={{ padding: '1.5rem 2rem' }}>
+   <nav className="flexBetween padding-container fixed top-0 w-full left-0 right-0 z-30 py-3" style={{ padding: '1rem 2rem', background:"#0B82CF", borderBottom:"1px solid #73BAE8" }}>
       <div className="flex gap-2 flex-row">
         <a href="#home">
           <div className="flex gap-2">
-          <Image src="/michuTech-logo_1.png" alt="logo" width={100} height={100} style={{ borderRadius: '0.25rem' }} />
-          <h3 className="text-blue-800 text-xl">MichuTech IT Solutions</h3>
+          <Image src="/michuLogo.png" alt="logo" width={50} height={50} style={{ borderRadius: '0.25rem' }} />
+          <div className="text-white text-xl">
+            <h1>MICHUTECH</h1>
+            <p>Connect to your future</p>
+          </div>
           </div>
         </a>
       </div>
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-          <li key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+          <li key={link.key} className="regular-16 text-white flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
           <a href={`${link.href}`} onClick={() => scrollToSection(link.id)}>
             {link.label}
           </a>
