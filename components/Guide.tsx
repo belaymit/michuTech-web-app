7e0aa66c83e6c4bfd04240da43e8d01fb8e8fa74
belaymit <2belamit@gmail.com>
2024-01-19 23:56:00 +0300

    Changed text color

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -6,12 +6,12 @@ export default function Guide(){
     <section className="flexCenter flex-col">
       <div className="padding-container max-container w-full pb-24">
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
-        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+        <p className="uppercase regular-20 -mt-1 mb-3 text-blue-700">
           We are here for you
         </p>
         <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Guide You to Easy Path</h2>
-          <p className="regular-16 text-gray-30 xl:max-w-[520px]">Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+          <h2 className="bold-40 lg:bold-52 xl:max-w-[420px]">To Guide You to <span style={{fontStyle:"italic", color:"#0B82CF"}}>Connect to your Future</span></h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[720px]" style={{textAlign:"justify"}}><span className="text-3xl" style={{color:"#0B82CF"}}>At MichuTech IT Solutions PLC</span> , we serve as your bridge to a future enhanced by digital solutions and cutting-edge application development. Our offerings include mobile and desktop applications, medical solutions, and expertise in AI and machine learning. Additionally, we provide consultancy services to guide you through the application development process, along with training programs focused on web development. With MichuTech's support, you can navigate diverse landscapes confidently, thanks to offline map capabilities for uninterrupted exploration in remote areas. Invite your friends and family to embark on thrilling adventures through valleys and to the summit of mountains, all facilitated by MichuTech's state-of-the-art solutions</p>
         </div>
       </div>
 
@@ -35,15 +35,15 @@ export default function Guide(){
           <div className="flexBetween flex-col">
             <div className='flex w-full flex-col'>
               <div className="flexBetween w-full">
-                <p className="regular-16 text-gray-20">Destination</p>
-                <p className="bold-16 text-green-50">48 min</p>
+                <p className="regular-16 text-gray-20">Project Completion</p>
+                <p className="bold-16 text-green-50 ml-4">Beta Release</p>
               </div>
-              <p className="bold-20 mt-2">Aguas Calientes</p>
+              <p className="bold-20 mt-2">Application Development</p>
             </div>
 
             <div className='flex w-full flex-col'>
-              <p className="regular-16 text-gray-20">Start track</p>
-              <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
+              <p className="regular-16 text-gray-20">Project Design</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Document Development</h4>
             </div>
           </div>
         </div>
