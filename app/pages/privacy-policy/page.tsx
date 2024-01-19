dd9704452415f70d883df8ec380b64896a362e5d
belaymit <2belamit@gmail.com>
2024-01-19 23:53:53 +0300

    Added privacy policy page

---
+++ app/pages/privacy-policy/page.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="min-h-screen flex items-center justify-center mt-24">
+      <div className="max-w-6xl px-4 sm:px-6 lg:px-8 py-6 bg-white rounded-md w-full sm:w-3/4 md:w-1/2">
+        <h1 className="text-3xl font-bold mb-6 text-center">Privacy Policy for MichuTech IT Solutions PLC</h1>
+        <p className="mb-4">
+          Welcome to MichuTech IT Solutions. This Privacy Policy outlines how we collect, use, disclose, and safeguard your personal information when you interact with our services, including software development, consultancy, design, and software development training.
+        </p>
+        <h2 className="text-2xl font-bold mt-6 mb-2">1. Information We Collect:</h2>
+        <ul className="list-disc pl-8 mb-4">
+          <li>
+            <p className="mb-2">1.1 Information You Provide:</p>
+            <p className="mb-2">We collect information you voluntarily provide when using our services, such as your name, contact information, and any additional details necessary for the specific service you engage with.</p>
+          </li>
+          <li>
+            <p className="mb-2">1.2 Automatically Collected Information:</p>
+            <p className="mb-2">When you use our services, we may automatically collect certain information, including but not limited to device information, log data, and analytics. This information helps us improve our services and user experience.</p>
+          </li>
+        </ul>
+        <h2 className="text-2xl font-bold mb-2">2. How We Use Your Information:</h2>
+        <ul className="list-disc pl-8 mb-4">
+            <li>
+              <p className="mb-2">2.1 Service Delivery:</p>
+              <p className="mb-2">We use the collected information to deliver and enhance the quality of our software development, consultancy, design, and training services.</p>
+            </li>
+            <li>
+              <p className="mb-2">2.2 Communication:</p>
+              <p className="mb-2">We may use your contact information to communicate with you about our services, updates, and respond to your inquiries.</p>
+            </li>
+          </ul>
+          <h2 className="text-2xl font-bold mb-2">  3. Sharing Your Information:</h2>
+          <ul className="list-disc pl-8 mb-4">
+          <li>
+            <p className="mb-2">We do not sell, trade, or rent your personal information to third parties. We may share your information with trusted service providers who assist us in delivering our services, but they are obligated to keep your information confidential.</p>
+          </li>
+          </ul>
+            <h2 className="text-2xl font-bold mb-2">4. Security:</h2>
+            <ul className="list-disc pl-8 mb-4">
+              <li>
+                <p className="mb-2">We prioritize the security of your personal information and implement measures to safeguard it. However, no method of transmission over the internet or electronic storage is entirely secure, and we cannot guarantee absolute security.</p>
+              </li>
+            </ul>
+            <h2 className="text-2xl font-bold mb-2">5. Cookies and Similar Technologies:</h2>
+            <ul className="list-disc pl-8 mb-4">
+            <li>
+              <p className="mb-2">We may use cookies and similar technologies to enhance your experience, analyze usage patterns, and personalize content. You can manage cookie preferences through your browser settings.</p>
+            </li>
+          </ul>
+            <h2 className="text-2xl font-bold mb-2">6. Your Choices:</h2>
+            <ul className="list-disc pl-8 mb-4">
+          <li>
+            <p className="mb-2">You can control and update your personal information through your account settings. If you wish to opt-out of certain communications, please follow the provided instructions.</p>
+          </li>
+          </ul>
+            <h2 className="text-2xl font-bold mb-2">7. Children's Privacy:</h2>
+             <ul className="list-disc pl-8 mb-4">
+          <li>
+            <p className="mb-2">Our services are not directed at individuals under the age of 13. We do not knowingly collect personal information from children. If you are a parent or guardian and believe your child has provided us with information, please contact us.</p>
+          </li>
+          </ul>
+            <h2 className="text-2xl font-bold mb-2">8. Changes to This Policy:</h2>
+           <ul className="list-disc pl-8 mb-4">
+          <li>
+            <p className="mb-2">We reserve the right to update or modify this Privacy Policy. Your continued use of our services after any changes indicates your acceptance of the revised policy.</p>
+          </li>
+          </ul>
+            <h2 className="text-2xl font-bold mb-2">9. Contact Us:</h2>
+           <ul className="list-disc pl-8 mb-4">
+          <li>
+            <p className="mb-2">If you have any questions, concerns, or requests regarding this Privacy Policy, please contact us at <a href="mailto:michutechitsolutionsplc@gmail.com"
+              className="text-blue-600 hover:underline">
+            </a></p>
+          </li>
+        </ul>
+
+        <p className="mt-8 text-gray-500 text-center">
+          Last updated: January 1, 2024
+        </p>
+      </div>
+    </div>
+  )
+}
