0679faa6a748a53087433d5854d6df576b2dd01f
belaymit <2belamit@gmail.com>
2024-01-22 21:56:06 +0300

    Reordered imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next';
-
-import './globals.css'
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'MichuTech App',
